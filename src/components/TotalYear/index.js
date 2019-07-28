ae00a3b43c7c227cada574ada278161b8097b2c4
AmyNguyen1130 <huongntt.htactive@gmail.com>
2019-07-27 19:42:59 -0700

    final edit

--- src/components/TotalYear/index.js
+++ src/components/TotalYear/index.js
@@ -6,11 +6,11 @@ export default class StatisticYear extends Component {
   render() {
     return (
       <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h3>Chart showing revenue for every months in 2019 (VND)</h3>
+        <h3>Chart showing revenue for every month in 2019 (VND)</h3>
         <Revenue />
         <br />
         <h3>
-          Chart showing register, pay for day, pay for month in 2019 (person)
+        Chart showing registered, paid for day, paid for month in 2019 (person)
         </h3>
         <ChartJS />
       </div>
