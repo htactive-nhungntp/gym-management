cc9bbde1351c999b112282d5eaafefdddb8573d9
Nam Dev <namdevdk2710@gmail.com>
2019-07-28 13:33:02 +0700

    Fix login

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -35,110 +35,90 @@ class SignUpBase extends Component {
   };
 
   checkLogin = event => {
-    event.preventDefault()
+    event.preventDefault();
 
-    let {username, password, user} = this.state
+    let { username, password, user } = this.state;
 
     if (user.user_name !== username && user.password !== password) {
-      alert("Username or Password is wrong")
+      alert("Username or Password is wrong");
     } else {
       localStorage.setItem("user", JSON.stringify(user));
-      this.props.history.push('/')
+      this.props.history.push("/");
     }
   };
 
   render() {
     return (
-      
-      <div className="col-lg-9 col-md-12 col-sm-12 col-xs-12">
-        <div className="login-form-area mg-t-30 mg-b-15">
-          <div className="container-fluid">
-            <div className="row">
-              
-              <form id="adminpro-register-form" className="adminpro-form" onSubmit={this.checkLogin}>
-                <div className="col-lg-12">
-                  <div className="login-bg">
-                    <div className="row">
-                      <div className="col-lg-12">
-                        <div className="logo">
-                          <a href="/">
-                            <img src="img/logo/logo.png" alt="" />
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-lg-12">
-                        <div className="login-title">
-                          <h1>Login Form </h1>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="row">
-                      <div className="col-lg-4">
-                        <div className="login-input-head">
-                          <p>Username</p>
-                        </div>
-                      </div>
-                      <div className="col-lg-8">
-                        <div className="login-input-area">
-                          <input
-                            type="text"
-                            name="username"
-                            onChange={e => this.toggleChange(e, "user_name")}
-                          />
-                          <i className="fa fa-user login-user" />
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="row">
-                      <div className="col-lg-4">
-                        <div className="login-input-head">
-                          <p>Password</p>
-                        </div>
-                      </div>
-                      <div className="col-lg-8">
-                        <div className="login-input-area">
-                          <input
-                            type="password"
-                            name="password"
-                            onChange={e => this.toggleChange(e, "password")}
-                          />
-                          <i className="fa fa-lock login-user" />
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="row">
-                      <div className="col-lg-4" />
-                      <div className="col-lg-8">
-                        <div className="login-button-pro">
-                          <button
-                            type="button"
-                            className="login-button login-button-lg"
-                            onClick={this.checkLogin}
-                          >
-                            Log In
-                          </button>
-                          <button
-                            type="submit"
-                            className="login-button login-button-rg"
-                          >
-                            Cancel
-                          </button>
-                        </div>
-                      </div>
+      <div className="container-fluid h-100vh">
+        <div className="row box-center">
+          <form
+            className="adminpro-form"
+            onSubmit={this.checkLogin}
+          >
+            <div className="col-lg-12">
+              <div className="login-bg">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="login-title text-center font-weight">
+                      <h1>Login Form </h1>
                     </div>
                   </div>
                 </div>
-              </form>
+
+                <div className="row">
+                  <div className="col-lg-4">
+                    <div className="login-input-head">
+                      <p>Username</p>
+                    </div>
+                  </div>
+                  <div className="col-lg-8">
+                    <div className="login-input-area">
+                      <input
+                        type="text"
+                        name="username"
+                        onChange={e => this.toggleChange(e, "user_name")}
+                      />
+                      <i className="fa fa-user login-user" />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-lg-4">
+                    <div className="login-input-head">
+                      <p>Password</p>
+                    </div>
+                  </div>
+                  <div className="col-lg-8">
+                    <div className="login-input-area">
+                      <input
+                        type="password"
+                        name="password"
+                        onChange={e => this.toggleChange(e, "password")}
+                      />
+                      <i className="fa fa-lock login-user" />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-12">
+                    <div className="login-button-pro">
+                      <button
+                        type="button"
+                        className="login-button login-button-lg btn-block"
+                        onClick={this.checkLogin}
+                      >
+                        Log In
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
-     
     );
   }
 }
