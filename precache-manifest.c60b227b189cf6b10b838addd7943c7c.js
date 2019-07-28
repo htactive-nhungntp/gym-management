a35763fde5d64e4d519c406838934d0dfb233c9b
Nam Dev <namdevdk2710@gmail.com>
2019-07-28 13:45:31 +0700

    Updates

---
+++ precache-manifest.c60b227b189cf6b10b838addd7943c7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a93661e52efe17669eb30f194e0407",
+    "url": "/gym-management/index.html"
+  },
+  {
+    "revision": "7971d89c5fe0fa231082",
+    "url": "/gym-management/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e9c5e73feb6e2c9376ae",
+    "url": "/gym-management/static/css/main.7ef435e9.chunk.css"
+  },
+  {
+    "revision": "7971d89c5fe0fa231082",
+    "url": "/gym-management/static/js/2.50ca549f.chunk.js"
+  },
+  {
+    "revision": "e9c5e73feb6e2c9376ae",
+    "url": "/gym-management/static/js/main.f34684e1.chunk.js"
+  },
+  {
+    "revision": "691e89a8d0039c14cd58",
+    "url": "/gym-management/static/js/runtime~main.f9b940d9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74e8568fcfe05c9ddbbe47f1d9c1f8bf.js"
+  "/gym-management/precache-manifest.c60b227b189cf6b10b838addd7943c7c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gym-management/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
