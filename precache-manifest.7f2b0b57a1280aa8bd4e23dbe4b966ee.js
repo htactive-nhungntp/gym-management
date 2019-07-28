fed66799a733a7ec35ff13fc5dc66b80c19a65fb
Nam Dev <namdevdk2710@gmail.com>
2019-07-28 12:47:36 +0700

    Updates

---
+++ precache-manifest.7f2b0b57a1280aa8bd4e23dbe4b966ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f1c10d2c71a41e9ce324f20a171be9",
+    "url": "/gym-management/index.html"
+  },
+  {
+    "revision": "7971d89c5fe0fa231082",
+    "url": "/gym-management/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "468449c90d5f6aa061ca",
+    "url": "/gym-management/static/css/main.7ef435e9.chunk.css"
+  },
+  {
+    "revision": "7971d89c5fe0fa231082",
+    "url": "/gym-management/static/js/2.50ca549f.chunk.js"
+  },
+  {
+    "revision": "468449c90d5f6aa061ca",
+    "url": "/gym-management/static/js/main.0a8f4abf.chunk.js"
+  },
+  {
+    "revision": "691e89a8d0039c14cd58",
+    "url": "/gym-management/static/js/runtime~main.f9b940d9.js"
+  }
+]);
