aedf7e0864d91a8f4b4e8736828d000111a793f1
Nam Dev <namdevdk2710@gmail.com>
2019-07-28 13:41:15 +0700

    Fix login

---
+++ build/precache-manifest.74e8568fcfe05c9ddbbe47f1d9c1f8bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f07fa72c80d0b9fac169bfbb9e2390",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cca278608ee839f0cbb",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "390d014a81133eed0eb2",
+    "url": "/static/css/main.7ef435e9.chunk.css"
+  },
+  {
+    "revision": "9cca278608ee839f0cbb",
+    "url": "/static/js/2.cd8651cf.chunk.js"
+  },
+  {
+    "revision": "390d014a81133eed0eb2",
+    "url": "/static/js/main.82f2a344.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
